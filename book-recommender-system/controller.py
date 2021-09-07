0714577ef186d13b08d925b46c079d85972f5a8e
Shubham Nawani <shubhamnawani99@gmail.com>
2021-09-07 20:12:43 +0530

    V0.1 basic RecSys, Frontend and flask integration

--- book-recommender-system/controller.py
+++ book-recommender-system/controller.py
@@ -1,22 +1,47 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, redirect, render_template, request, url_for
 )
-from werkzeug.exceptions import abort
 
 from .db import get_db
+from .recommender import main
 
 bp = Blueprint('recommendation', __name__)
 
+current_list = dict()
+
 
 # end point "index" as defined using add_url_rule in __init__
-@bp.route('/')
+@bp.route('/', methods=('GET', 'POST'))
 def index():
+    if request.method == 'POST':
+
+        # fill the current dictionary with book title and ratings
+        if 'select-form' in request.form:
+
+            books_selected = request.form.getlist("book-choice")
+            book_ratings = request.form.get("book-rating")
+
+            for book, rating in zip(books_selected, book_ratings):
+                current_list[book] = int(rating)
+
+        # process the form
+        elif 'process-form' in request.form:
+
+            result = main(current_list)
+            current_list.clear()
+
+            return render_template('recommendation-system/process.html', result=result)
+
+    # load the book list into the select input from the database
     db = get_db()
-    posts = db.execute(
-        'SELECT book_id, authors, title from books'
+    books = db.execute(
+        'SELECT book_id, authors, title from books limit 10'
     ).fetchall()
 
-    return render_template('recommendation-system/index.html', posts=posts)
+    length = len(current_list) | 0
+
+    # render the index template
+    return render_template('recommendation-system/index.html', books=books, current_list=current_list,length=length)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
@@ -33,7 +58,6 @@ def create():
             flash(error)
         else:
             # the call to recommender system goes here...
-            print(title, body)
             return redirect(url_for('recommendation.index'))
 
     return render_template('recommendation-system/create.html')
---
+++ book-recommender-system/recommender.py
@@ -0,0 +1,85 @@
+import pandas as pd
+# Pearson correlation
+from scipy.stats import pearsonr
+
+from .db import get_db
+
+
+def user_colab(user, ratings_df, books_df):
+    # Create dataframe for new user (me)
+    user = pd.DataFrame(columns=['title', 'rating'], data=user.items())
+
+    # Add book_id from books_df
+    new_user = pd.merge(user, books_df, on='title', how='inner')
+    new_user = new_user[['book_id', 'title', 'rating']].sort_values(by='book_id')
+
+    other_users = ratings_df[ratings_df['book_id'].isin(new_user['book_id'].values)]
+
+    other_users['user_id'].nunique()
+
+    # Sort users by count of most mutual books with me
+    users_mutual_books = other_users.groupby(['user_id'])
+    users_mutual_books = sorted(users_mutual_books, key=lambda x: len(x[1]), reverse=True)
+
+    # Get the top 100 mutual users
+    top_users = users_mutual_books[:100]
+
+    # Initialize the pearson correlation
+    pearson_corr = {}
+
+    for user_id, features in top_users:
+        # Books should be sorted
+        features = features.sort_values(by='book_id')
+        features_list = features['book_id'].values
+
+        new_user_ratings = new_user[new_user['book_id'].isin(features_list)]['rating'].values
+        user_ratings = features[features['book_id'].isin(features_list)]['rating'].values
+
+        corr = pearsonr(new_user_ratings, user_ratings)
+        pearson_corr[user_id] = corr[0]
+
+    # Get top50 users with the highest similarity indices
+    pearson_df = pd.DataFrame(columns=['user_id', 'similarity_index'], data=pearson_corr.items())
+    pearson_df = pearson_df.sort_values(by='similarity_index', ascending=False)[:50]
+
+    # Get all books for these users and add weighted book's ratings
+    users_rating = pearson_df.merge(ratings_df, on='user_id', how='inner')
+    users_rating['weighted_rating'] = users_rating['rating'] * users_rating['similarity_index']
+
+    # Calculate sum of similarity index and weighted rating for each book
+    grouped_ratings = users_rating.groupby('book_id').sum()[['similarity_index', 'weighted_rating']]
+
+    recommend_books = pd.DataFrame()
+
+    # Add average recommendation score
+    recommend_books['avg_recommend_score'] = grouped_ratings['weighted_rating'] / grouped_ratings['similarity_index']
+    recommend_books['book_id'] = grouped_ratings.index
+    recommend_books = recommend_books.reset_index(drop=True)
+
+    # Books with the highest score
+    recommend_books = recommend_books[(recommend_books['avg_recommend_score'] == 5)]
+
+    # Top-10 Recommendations
+    recommendation = books_df[books_df['book_id'].isin(recommend_books['book_id'])][
+        ['authors', 'title']].sample(10)
+
+    return recommendation.to_dict('records')
+
+
+def main(user_info):
+    # get the database instance
+    db = get_db()
+
+    # create the dataframes
+    books_df = pd.read_sql_query("SELECT * FROM books", db)
+    ratings_df = pd.read_sql_query("SELECT * FROM ratings", db)
+
+    # run the recommendation system
+    recommendation = user_colab(user_info, ratings_df, books_df)
+
+    # return the recommendations
+    return recommendation
+
+
+if __name__ == "__main__":
+    main()
